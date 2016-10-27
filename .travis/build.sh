fd50eb8adf9ee3cfc57c142be6e384d4c4866575
John Freeman <john.freeman.opensource@gmail.com>
2016-10-27 11:29:20 +0100

    Omit the symbol table and debug information
    Make the binaries smaller by omitting the symbol table and debug information.

--- .travis/build.sh
+++ .travis/build.sh
@@ -10,7 +10,8 @@ echo
 
 gox -ldflags="-X main.version=${TRAVIS_TAG:-unknown} \
               -X main.revision=${TRAVIS_COMMIT:-unknown} \
-              -X main.built=$(date --iso-8601=seconds)" \
+              -X main.built=$(date --iso-8601=seconds) \
+              -s" \
     -os="${OS_TARGETS:-linux darwin windows}" \
     -arch="${ARCH_TARGETS:-amd64}" \
     -output="dist/{{.OS}}/{{.Arch}}/{{.Dir}}"
