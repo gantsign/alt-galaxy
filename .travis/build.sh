97f74bd80caf301135ab26964d5b36092b3761a6
John Freeman <john.freeman.opensource@gmail.com>
2016-10-27 11:23:28 +0100

    Set version details as part of build (#2)
    The linker can overwrite strings; this feature is used to set the version, revision and time of build for the version string.

--- .travis/build.sh
+++ .travis/build.sh
@@ -8,6 +8,9 @@ echo '# Building binaries #'
 echo '#####################'
 echo
 
-gox -os="${OS_TARGETS:-linux darwin windows}" \
+gox -ldflags="-X main.version=${TRAVIS_TAG:-unknown} \
+              -X main.revision=${TRAVIS_COMMIT:-unknown} \
+              -X main.built=$(date --iso-8601=seconds)" \
+    -os="${OS_TARGETS:-linux darwin windows}" \
     -arch="${ARCH_TARGETS:-amd64}" \
     -output="dist/{{.OS}}/{{.Arch}}/{{.Dir}}"
